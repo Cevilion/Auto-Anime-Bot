d1dedbfe257f3d1c4e10e0f5d44d4ef7c0ee0fd6
Cevilion <sumitsajwan135@gmail.com>
2025-03-27 09:50:07 +0530

    Update ffencoder.py

--- bot/core/ffencoder.py
+++ bot/core/ffencoder.py
@@ -16,7 +16,7 @@ ffargs = {
     '1080': Var.FFCODE_1080,
     '720': Var.FFCODE_720,
     '480': Var.FFCODE_480,
-    'Hdrip': Var.FFCODE_Hdrip,
+    'Hdri': Var.FFCODE_Hdri,
 }
 
 class FFEncoder:
